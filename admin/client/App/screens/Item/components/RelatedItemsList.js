5abdc7f0db5a9399a971ff358a357d4fbeff2815
Wout Mertens <Wout.Mertens@gmail.com>
2016-08-24 10:57:52 +0200

    Webpack: Build signin and admin UI bundles
    skipped some fields for now

--- admin/client/App/screens/Item/components/RelatedItemsList.js
+++ admin/client/App/screens/Item/components/RelatedItemsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Columns } from 'FieldTypes';
+import { Columns } from '../../../../../../fields';
 import { Alert, Spinner } from 'elemental';
 
 const RelatedItemsList = React.createClass({
